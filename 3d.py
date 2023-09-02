659655918a3ce0443c2f9a37fcd1005e928e812d
sayuz394 <71702420+sayuz394@users.noreply.github.com>
2023-09-02 17:45:04 +0700

    Add files via upload

---
+++ 3d.py
@@ -0,0 +1,30 @@
+from turtle import *
+a = 0
+while a < 5:
+   a += 1
+   speed(100)
+   dot(100)
+   right(100)
+   circle(1000)
+   circle(10000)
+   circle(100001)
+   left(1)
+   circle(100000)
+   left(1)
+   circle(100000)
+   left(1)
+   circle(100000)
+   left(1)
+   circle(100000)
+   left(1)
+   circle(100000)
+   left(1)
+   circle(100000)
+   left(1)
+   circle(100000)
+   break
+
+input("")
+
+
+
